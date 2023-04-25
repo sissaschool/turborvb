48f39efd1ef2f0be1b32776b01e979361b4b695c
Otto Kohulak <pravod@gmail.com>
2023-04-25 17:57:43 +0200

    Fix shebangs in bin directory, Fix permissions

--- bin/force.py
+++ bin/force.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Copyright (C) 2022 TurboRVB group
 #
 # This program is free software: you can redistribute it and/or modify
--- bin/plot_NormCorr.sh
+++ bin/plot_NormCorr.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Copyright (C) 2022 TurboRVB group
 #
 # This program is free software: you can redistribute it and/or modify
