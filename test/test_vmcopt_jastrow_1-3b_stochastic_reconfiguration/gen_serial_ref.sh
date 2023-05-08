dba6cc144352a23a386144cdc6e99b6d29558ade
kousuke-nakano <37653569+kousuke-nakano@users.noreply.github.com>
2023-05-08 11:39:07 +0900

    added a new test, vmcopt jastrow 1-3 bodies with stochastic reconfiguration

---
+++ test/test_vmcopt_jastrow_1-3b_stochastic_reconfiguration/gen_serial_ref.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+TURBORVB="../../bin/turborvb-serial.x"
+READF="../../bin/readf.x"
+FORT21=REFERENCE_fortXXI
+OUT=out_true.o
+
+debug_root=`pwd`
+
+cd $debug_root
+cp fort.10_ori fort.10
+$TURBORVB < datasmin.d > $OUT
+echo "0 1 1 1" | $READF >& /dev/null 
+cp fort.21 ${FORT21}
+cd $debug_root
